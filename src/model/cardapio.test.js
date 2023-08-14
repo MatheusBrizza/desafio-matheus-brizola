f3294fe190c70dff2f9f4439d823d1760261af70
Matheus <matheusbj95@gmail.com>
2023-08-14 20:27:08 -0300

    testes criados para classe Cardápio

--- src/model/cardapio.test.js
+++ src/model/cardapio.test.js
@@ -1,15 +1,19 @@
 import { Cardapio } from "./cardapio.js"
 
 describe('Cardapio', () => {
+    test("getCodigo deve retornar cafe", () => {
+        const resultado = new Cardapio('cafe', 'Café', 'R$3,00');
+        expect(resultado.getCodigo).toEqual('cafe');
+    })
+    
+    test("getValor deve retornar R$3,00", () => {
+        const resultado = new Cardapio('chantily', 'Chantily', 'R$1,50');
+        expect(resultado.getValor).toEqual('R$1,50');
+    })
 
-    const validaTeste = (codigo, descricao, valor) => {
-        const resultado = new Cardapio(codigo, descricao, valor);
-        
-        expect(resultado.replace("\xa0", " ")).toEqual(resultado);
-    }
+    test("getDescricao deve retornar Sanduíche", () => {
+        const resultado = new Cardapio('sanduiche', 'Sanduíche', "R$6,50");
+        expect(resultado.descricao).toEqual('Sanduíche');
+    })
 
-    test.each([
-        ['cafe', 'Cafe', '3', []]
-    ])('compra %p em %p deve resultar em %p', (_, formaDePagamento, resultadoEsperado, itens) =>
-        validaTeste('cafe', 'cafe', '3'));
 })
