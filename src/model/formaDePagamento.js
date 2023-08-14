baa0afa065554d6cedaa793acb8ce976603f04a5
Matheus <matheusbj95@gmail.com>
2023-08-14 20:25:09 -0300

    regra de desconto e taxa adicionada

--- src/model/formaDePagamento.js
+++ src/model/formaDePagamento.js
@@ -10,12 +10,25 @@ class FormaDePagamento {
    
     get getCurrentPagamentoStrategy() {
         return this.currentPagamentoStrategy;
-      }
+    }
   
     set setPagamentoStrategy(strategy) {
       this.currentPagamentoStrategy = this.pagamentoStrategy[strategy];
     }
   
+    pagamento(valor) {
+        if (!this.currentPagamentoStrategy) {
+            console.log('Forma de pagamento inválida.');
+            return;
+        }
+      
+        if (this.currentPagamentoStrategy instanceof DinheiroStrategy) {
+            valor *= 0.95;
+        } else if (this.currentPagamentoStrategy instanceof CreditoStrategy) {
+            valor *= 1.03;
+        }
+        return valor;
+    }
 }
 
 export { FormaDePagamento }
