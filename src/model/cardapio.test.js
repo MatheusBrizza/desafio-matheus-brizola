0c1adbe05d87ec29b271091a4bb55ed61ef2ca23
Matheus <matheusbj95@gmail.com>
2023-08-15 14:34:33 -0300

    refeito os testes para testar todos atributos juntos

--- src/model/cardapio.test.js
+++ src/model/cardapio.test.js
@@ -1,19 +1,11 @@
 import { Cardapio } from "./cardapio.js"
 
 describe('Cardapio', () => {
-    test("getCodigo deve retornar cafe", () => {
+    test("deve retornar um cardápio válido", () => {
         const resultado = new Cardapio('cafe', 'Café', 'R$3,00');
         expect(resultado.getCodigo).toEqual('cafe');
+        expect(resultado.getDescricao).toEqual('Café');
+        expect(resultado.getValor).toEqual('R$3,00');
     })
     
-    test("getValor deve retornar R$3,00", () => {
-        const resultado = new Cardapio('chantily', 'Chantily', 'R$1,50');
-        expect(resultado.getValor).toEqual('R$1,50');
-    })
-
-    test("getDescricao deve retornar Sanduíche", () => {
-        const resultado = new Cardapio('sanduiche', 'Sanduíche', "R$6,50");
-        expect(resultado.descricao).toEqual('Sanduíche');
-    })
-
 })
